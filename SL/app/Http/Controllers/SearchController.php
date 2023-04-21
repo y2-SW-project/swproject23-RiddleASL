bb6fc163ec281541c51d54727507e75a3996287b
RiddleASL <n00212142@iadt.ie>
2023-04-21 13:58:28 +0100

    validation

--- SL/app/Http/Controllers/SearchController.php
+++ SL/app/Http/Controllers/SearchController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class SearchController extends Controller
 {
@@ -13,26 +14,56 @@ class SearchController extends Controller
         if($request->ajax()){
             $output="";
             $words= DB::table('words')->where('word','LIKE','%'.$request->search."%")->paginate(8)->sortBy('word');
-            if($words){
-                foreach ($words as $key => $word) {
-                    if($word->show == 1){
-                        $info = '';
-                        if(strlen($word->info) > 20){
-                            $info = substr($word->info, 0,20).'...';
-                        } else{
-                            $info = $word->info;
+            
+            if($words){  
+                if(Auth::user()){
+                    if(auth()->user()->isAdmin){
+                            foreach ($words as $key => $word) {
+                                if($word->show == 1){
+                                    $info = '';
+                                    if(strlen($word->info) > 20){
+                                        $info = substr($word->info, 0,20).'...';
+                                    } else{
+                                        $info = $word->info;
+                                    }
+                                    $output.='<tr>'.
+                                    '<td> <a href='.route('words.show', $word->id).' class="text-white text-decoration-none fs-5">'.$word->word.'</a> <span class="text-bg-l fs-5"> - '.$info.'</span></td>'.
+                                    '</tr>';
+                                } else if($word->show == 0){
+                                    $info = '';
+                                    if(strlen($word->info) > 20){
+                                        $info = substr($word->info, 0,20).'...';
+                                    } else{
+                                        $info = $word->info;
+                                    }
+                                    $output.='<tr>'.
+                                    '<td> <a href='.route('words.show', $word->id).' class="text-danger text-decoration-none fs-5">'.$word->word.'</a> <span class="text-bg-l fs-5"> - '.$info.'</span></td>'.
+                                    '</tr>';
+                            } 
                         }
-                        $output.='<tr>'.
-                        '<td> <a href='.route('words.show', $word->id).' class="text-white text-decoration-none fs-5" data-id='.$word->id.'>'.$word->word.'</a> <span class="text-bg-l fs-5"> - '.$info.'</span></td>'.
-                        '</tr>';
+                        return Response($output);
+
                     }
-                }
-                
-                return Response($output);
+                } else {
+                    foreach ($words as $key => $word) {
+                        if($word->show == 1){
+                            $info = '';
+                            if(strlen($word->info) > 20){
+                                $info = substr($word->info, 0,20).'...';
+                            } else{
+                                $info = $word->info;
+                            }
+                            $output.='<tr>'.
+                            '<td> <a href='.route('words.show', $word->id).' class="text-white text-decoration-none fs-5">'.$word->word.'</a> <span class="text-bg-l fs-5"> - '.$info.'</span></td>'.
+                            '</tr>';
+                        }
+                        
+                    }
+                    return Response($output);
             }
         }
     }
-
+}
     public function result(Request $request)
     {
         if($request->ajax()){
